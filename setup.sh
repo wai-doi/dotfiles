893a67b81fee682d949ee3e49f18dc6d2440ed30
Yusuke Doi <yuusukedoi23@gmail.com>
2021-07-10 06:10:04 +0900

    update setup.sh
    config全体をシンボリックリンクにしない
    
    https://qiita.com/gsy0911/items/159b0fe42daf36785e7e#%E3%82%B7%E3%83%B3%E3%83%9C%E3%83%AA%E3%83%83%E3%82%AF%E3%83%AA%E3%83%B3%E3%82%AF%E3%82%92%E8%B2%BC%E3%82%8B

--- setup.sh
+++ setup.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
-DOTPATH=${PWD}
-for f in .??*
-do
-    [ "$f" = ".git" ] && continue
-    ln -snfv "$DOTPATH/$f" "$HOME"/"$f"
+IGNORE_PATTERN="^\.(git|config)"
+
+echo "Create dotfile links."
+
+for dotfile in .??*; do
+    [[ $dotfile =~ $IGNORE_PATTERN ]] && continue
+    ln -snfv "$(pwd)/$dotfile" "$HOME/$dotfile"
 done
+
+# create .config in $HOME
+mkdir -p $HOME/.config
+for dotfile in "$(ls .config)"; do
+    [[ $dotfile =~ $IGNORE_PATTERN ]] && continue
+    ln -snfv "$(pwd)/.config/$dotfile" "$HOME/.config/$dotfile"
+done
+
+echo "Success"
